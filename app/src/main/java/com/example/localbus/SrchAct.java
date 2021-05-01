6fc804caf4b154177f4b0a91a18cadd1e01c22cd
RockyUmesh <rocky11brooklyn10@gmail.com>
2021-05-01 14:04:56 +0530

    THE LOCAL BUS APK

--- app/src/main/java/com/example/localbus/SrchAct.java
+++ app/src/main/java/com/example/localbus/SrchAct.java
@@ -6,7 +6,6 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.constraintlayout.widget.ConstraintLayout;
-
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
